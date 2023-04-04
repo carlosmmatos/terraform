2767500e46c5340e3708d81751c528e7787f4199
Carlos Matos <carlos.matos@crowdstrike.com>
2023-04-04 13:03:52 -0400

    Latest changes for ava demo

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -43,6 +43,13 @@ resource "aws_security_group" "this" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["${local.ip}/32"]
+  }
 }
 
 module "ec2_instance" {
--- ec2_instance/outputs.tf
+++ ec2_instance/outputs.tf
@@ -1,4 +1,10 @@
-## Ip Address of the ec2 instance
-output "public_ip" {
+# Uncomment to get the b64 encoded password for Windows instances
+#
+#output "get_password_data" {
+#  value = module.ec2_instance.password_data
+#}
+
+# Public IP address of the instance
+output "get_public_ip" {
   value = module.ec2_instance.public_ip
 }
