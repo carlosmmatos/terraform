934419b0cba278b05c90b0fa9ded86e8e075671d
Carlos Matos <carlos.matos@crowdstrike.com>
2023-05-05 12:30:12 -0400

    Updates for ava tf

--- ec2_instance/outputs.tf
+++ ec2_instance/outputs.tf
@@ -1,8 +1,8 @@
 # Uncomment to get the b64 encoded password for Windows instances
 #
-#output "get_password_data" {
-#  value = module.ec2_instance.password_data
-#}
+output "get_password_data" {
+ value = module.ec2_instance.password_data
+}
 
 # Public IP address of the instance
 output "get_public_ip" {
